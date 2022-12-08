1fc1d3a2b40def8ed8ad56bc55988224be8907c1
Vadzim.Narkevich <Vadzim.Narkevich@itechart.com>
2022-12-08 21:52:43 +0300

    Add home page

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,10 +1,71 @@
 import { Container } from "@mui/system";
+import { useNavigate } from "react-router-dom";
+import classNames from "classnames";
+
+import { useRoleAppConfig } from "common/hooks/useRoleAppConfig";
+import { links, localization } from "resources";
+import { ProductType } from "models/domain/ProductType";
+
+import styles from './HomePage.module.scss';
+
+type ProductTypeCard = {
+    label: () => string;
+    productType: ProductType;
+}
+
+const productTypeCards: ProductTypeCard[] = [
+    {
+        label: () => localization.rolls,
+        productType: ProductType.Rolls,
+    },
+    {
+        label: () => localization.sushi,
+        productType: ProductType.Sushi,
+    },
+    {
+        label: () => localization.sets,
+        productType: ProductType.Sets,
+    },
+    {
+        label: () => localization.soups,
+        productType: ProductType.Soups,
+    },
+    {
+        label: () => localization.noodles,
+        productType: ProductType.Noodles,
+    },
+    {
+        label: () => localization.snacks,
+        productType: ProductType.Snacks,
+    },
+    {
+        label: () => localization.drinks,
+        productType: ProductType.Drinks,
+    },
+    {
+        label: () => localization.garnish,
+        productType: ProductType.Garnish,
+    },
+];
 
 export function Home() {
+    const navigate = useNavigate();
+    const { menuLinksBuilder } = useRoleAppConfig();
+
     return (
         <Container>
-            <div>
-
+            <div className={styles['home-page']}>
+                {productTypeCards.map(p => {
+                    const linkTo = menuLinksBuilder?.(p.productType) ?? links.root;
+                    return (
+                        <div
+                            className={classNames(styles['product-image'], styles[`product-image-${p.productType}`])}
+                            onClick={() => navigate(linkTo)}
+                        >
+                            {p.label()}
+                        </div>
+                    )
+                })}
             </div>
         </Container>
     );
